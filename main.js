051cafb94b6cd5d646058c24ea6f2ccabcea38d4
Pavlos Makridis <pavlosmakridis1322001@gmail.com>
2019-06-28 20:24:17 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-//Main javascript file 
+//Main javascript file
+function changeBackground(){
+  var color = document.getElementById("color_field").value;
+  //alert(color);
+  document.body.style.backgroundColor = color;
+}

1bb62dc4a18416cae540038abfce912944b5bc6e
Alexandros Bartzos <abartzos@gmail.com>
2019-06-28 21:04:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 //Main javascript file
+function changeBackground(){
+    var color = document.getElementById("color_field").value;
+     //alert(color);
+    document.body.style.backgroundColor = color;
+}
+
+
+//FOR TESTING PURPOSES
+//creates a bunch of entities just to test
+entities = []
+for (var i = 0; i < 100; i++) {
+	entities.push([Math.floor(Math.random() * 255),
+				   Math.floor(Math.random() * 255),
+				   Math.floor(Math.random() * 255)]);
+}
+
+console.log(entities);
+
+class Entity {
+	constructor(color){
+		// creates an entity
+		this.position = [Math.floor(Math.random() * 200),
+						 Math.floor(Math.random() * 200)];
+		this.color = color;
+	}
+}
+
+function generation(entities) {
+	for (var i = 0; i < entities.length; i++) {
+		
+	}
+}
 
 var mutation_chance;
 

ae594d77f159a58587f13b2487d90aa6514fe7df
Pavlos Makridis <pavlosmakridis1322001@gmail.com>
2019-06-28 20:58:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 //Main javascript file
-function changeBackground(){
-  var color = document.getElementById("color_field").value;
-  //alert(color);
-  document.body.style.backgroundColor = color;
+
+var mutation_chance;
+
+function initialize(){
+    var color = document.getElementById("color_field").value;
+    //alert(color);
+    document.body.style.backgroundColor = color;
+    var mutation_rate = document.getElementById("mutation_field").value;
+    mutation_chance = mutation_rate;
 }

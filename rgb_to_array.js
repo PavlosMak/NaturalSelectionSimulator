def8177dec60dca33a55cd46b79bf970b5b8fa59
PavlosMak <pavlosmakridis1322001@gmail.com>
2019-06-29 13:48:18 +0300

    rgb_to_array.js now deals with 2 digits or 1 digit rgb color values

--- rgb_to_array.js
+++ rgb_to_array.js
@@ -1,10 +1,14 @@
 function rgb_to_array(){
-    var rgb_string = "RGB(233,134,344)";
+    var rgb_string = "RGB(2323,4,324)";
 
     //Get colors from RGB string as strings
-    var red = rgb_string.slice(4, 7);
-    var green = rgb_string.slice(8, 11);
-    var blue = rgb_string.slice(12,15);
+    var first_comma_position = rgb_string.search(',');
+    var red = rgb_string.slice(4, first_comma_position);
+    var without_red = rgb_string.slice(first_comma_position+1,rgb_string.length);
+
+    var second_comma_position = without_red.search(',');
+    var green = without_red.slice(0, second_comma_position+1);
+    var blue = without_red.slice(second_comma_position+1, without_red.length);
 
     //Changes color values from strings to ints
     red = parseInt(red);
@@ -12,5 +16,6 @@ function rgb_to_array(){
     blue = parseInt(blue);
 
     var rgb_array = [red,green,blue];
-    alert(rgb_array);
+
+    //alert(rgb_array);
 }

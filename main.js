b65a01004b395ed287dc6b688a4df3b9a9bfcb70
PavlosMak <pavlosmakridis1322001@gmail.com>
2019-07-01 01:09:23 +0300

    Export log to CSV functionality

--- main.js
+++ main.js
@@ -248,3 +248,22 @@ function initialize() {
     //enables the generation button
     enable_button("generation");
 }
+
+//Export Log to CSV file
+$(document).ready(function () {
+   $('table').each(function () {
+       var $table = $(this);
+
+       var $button = $("<button type='button'>");
+       $button.text("Export to CSV");
+       $button.insertAfter($table);
+
+       $button.click(function () {
+           var csv = $table.table2CSV({
+               delivery: 'value'
+           });
+           window.location.href = 'data:text/csv;charset=UTF-8,'
+           + encodeURIComponent(csv);
+       });
+   });
+})
